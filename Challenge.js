e97e03c79ef6c5c330d646f524d6874c258e07d0
Propa Panda <halofreek45@gmail.com>
2016-11-16 11:27:45 -0800

    Finished 5 I think?

--- Challenge.js
+++ Challenge.js
@@ -1,22 +1,56 @@
 var character = document.getElementById("character");
 var charXValue = Number(character.getAttribute("x"));
 var charYValue = Number(character.getAttribute("y"));
-document.addEventListener("keydown", function(e){
-  if(e.keyCode == 37) {
-    charXValue = charXValue - 10;
-    character.setAttribute("x", charXValue);
-  }
-  else if(e.keyCode == 39) {
-    charXValue = charXValue + 10;
-    character.setAttribute("x", charXValue);
-  }
-  else if(e.keyCode == 40) {
-    charYValue = charYValue + 10;
-    character.setAttribute("y", charYValue);
-  }
-  else if(e.keyCode == 38) {
-    charYValue = charYValue - 10;
-    character.setAttribute("y", charYValue);
-  }
-  console.log(e.keyCode + "XVal = " + charXValue + " YVal = " + charYValue);
+var foodArray = [];
+var blueFish = document.getElementById("blueFish");
+var redFish = document.getElementById("redFish");
+var xCollision;
+var yCollision;
+var collision;
+foodArray.push(blueFish);
+foodArray.push(redFish);
+document.addEventListener("keydown", function(e) {
+    if (e.keyCode == 37) {
+        charXValue = charXValue - 10;
+        character.setAttribute("x", charXValue);
+    } else if (e.keyCode == 39) {
+        charXValue = charXValue + 10;
+        character.setAttribute("x", charXValue);
+    } else if (e.keyCode == 40) {
+        charYValue = charYValue + 10;
+        character.setAttribute("y", charYValue);
+    } else if (e.keyCode == 38) {
+        charYValue = charYValue - 10;
+        character.setAttribute("y", charYValue);
+    }
+
+    for (var i = 0; i < foodArray.length; i++) {
+        var currentFishX = Number(foodArray[i].getAttribute("x"));
+        var currentFishXEnd = currentFishX + Number(foodArray[i].getAttribute("width"));
+        var currentFishY = Number(foodArray[i].getAttribute("y"));
+        var currentFishYEnd = currentFishY + Number(foodArray[i].getAttribute("height"));
+
+        xCollision = false;
+        yCollision = false;
+
+        if (Number(character.getAttribute("x")) > currentFishX && Number(character.getAttribute("x")) < currentFishXEnd) {
+            console.log("Collision");
+            xCollision = true;
+        }
+        if (Number(character.getAttribute("y")) > currentFishY && Number(character.getAttribute("y")) < currentFishYEnd) {
+            console.log("Collision");
+            yCollision = true;
+        }
+        if (xCollision == true && yCollision == true) {
+            foodArray[i].setAttribute("x", getRandomInt(0, 700));
+        } else {
+            xCollision = false;
+            yCollision = false;
+        }
+    }
+    console.log(xCollision);
 })
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
