64f46f43b140131e2a6d0733f02a71816d393a18
Propa Panda <halofreek45@gmail.com>
2016-12-05 13:05:30 -0800

    Finished
    If you want to change the amount of enemys change the limit on the for
    loop at the top of Challenge.js

--- Challenge.js
+++ Challenge.js
@@ -16,22 +16,35 @@ var gamePaused = false;
 var timeStart = Date.now();
 var timeStop;
 var timeString = "";
+var winGameText;
 var gameOverText;
+var livesText = document.getElementById("livesText");
 var sharkEnemy = document.getElementById("shark");
-var sharkX;
-var sharkXEnd;
-var sharkY;
-var sharkYEnd;
+var lives = 10;
+var won;
+var enemyArray = [];
+
+enemyArray.push(sharkEnemy)
+for (var i = 0; i < 3; i++) {
+  var clone = sharkEnemy.cloneNode(true);
+  clone.setAttribute("id", "clone" + i);
+  clone.setAttribute("x", getRandomInt(50, window.innerWidth - 50));
+  clone.setAttribute("y", getRandomInt(50, window.innerHeight - 50));
+  enemyArray.push(clone);
+  mainCanvas.appendChild(clone);
+}
 
 blueFish.setAttribute("x", getRandomInt(50, window.innerWidth - 50));
 redFish.setAttribute("x", getRandomInt(50, window.innerWidth - 50));
 
+livesText.textContent = "Lives Left: " + lives.toFixed(0);
 scoreBoard.textContent = "Total Fish Eaten: 0"
 foodArray.push(blueFish);
 foodArray.push(redFish);
 document.addEventListener("keydown", function(e) {
     if (gamePaused == false) {
         if (totalFishEaten > 9) {
+            won = true;
             endGame();
         }
         if (e.keyCode == 37) {
@@ -58,7 +71,7 @@ function getRandomInt(min, max) {
 function checkForFoodCollision() {
     for (var i = 0; i < foodArray.length; i++) {
         var currentFishX = Number(foodArray[i].getAttribute("x"));
-        var currentFishXEnd = Number(foodArray[i].getAttribute("x"))+ 80;
+        var currentFishXEnd = Number(foodArray[i].getAttribute("x")) + 80;
         var currentFishY = Number(foodArray[i].getAttribute("y"));
         var currentFishYEnd = Number(foodArray[i].getAttribute("y")) + 160;
         xCollision1 = false;
@@ -81,17 +94,15 @@ function checkForFoodCollision() {
             foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
             totalFishEaten++;
         } else if (xCollision2 == true && yCollision2 == true) {
-          foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
-          totalFishEaten++;
-        } else if(xCollision1 == true && yCollision2 == true) {
-          foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
-          totalFishEaten++;
-        }
-        else if(xCollision2 == true && yCollision1 == true) {
-          foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
-          totalFishEaten++;
-        }
-         else {
+            foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
+            totalFishEaten++;
+        } else if (xCollision1 == true && yCollision2 == true) {
+            foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
+            totalFishEaten++;
+        } else if (xCollision2 == true && yCollision1 == true) {
+            foodArray[i].setAttribute("x", getRandomInt(50, window.innerWidth - 50));
+            totalFishEaten++;
+        } else {
             xCollision1 = false;
             yCollision1 = false;
             xCollision2 = false;
@@ -100,64 +111,84 @@ function checkForFoodCollision() {
     }
     window.requestAnimationFrame(checkForFoodCollision);
 }
+
 function checkForEnemyCollision() {
-  sharkX = Number(sharkEnemy.getAttribute("x"));
-  sharkXEnd = Number(sharkEnemy.getAttribute("x"))+ 80;
-  sharkY = Number(sharkEnemy.getAttribute("y"));
-  sharkYEnd = Number(sharkEnemy.getAttribute("y")) + 160;
-  xCollision3 = false;
-  yCollision3 = false;
-  xCollision4 = false;
-  yCollision4 = false;
-  if (Number(character.getAttribute("x")) > sharkX && Number(character.getAttribute("x")) < sharkXEnd) {
-      xCollision3 = true;
-  }
-  if (Number(character.getAttribute("y")) > sharkY && Number(character.getAttribute("y")) < sharkYEnd) {
-      yCollision3 = true;
-  }
-  if (Number(character.getAttribute("x")) + 80 < sharkXEnd && Number(character.getAttribute("x")) + 80 > sharkX) {
-      xCollision4 = true;
-  }
-  if (Number(character.getAttribute("y")) + 160 < sharkYEnd && Number(character.getAttribute("y")) + 160 > sharkY) {
-      yCollision4 = true;
-  }
-  if (xCollision3 == true && yCollision3 == true) { console.log("collision with shark")  }
-  else if (xCollision4 == true && yCollision4 == true) { console.log("collision with shark")   }
-  else if (xCollision3 == true && yCollision4 == true) { console.log("collosion with shark")   }
-  else if (xCollision4 == true && yCollision3 == true) { console.log("collision with shark")   }
-  else {
+    if(lives < 0) {
+      won = false;
+      endGame();
+    }
+    for(var i = 0; i < enemyArray.length; i++) {
+      var sharkX;
+      var sharkXEnd;
+      var sharkY;
+      var sharkYEnd;
+      var collision3;
+      var collision4;
+
+      sharkX = Number(enemyArray[i].getAttribute("x"));
+      sharkXEnd = Number(enemyArray[i].getAttribute("x")) + 80;
+      sharkY = Number(enemyArray[i].getAttribute("y"));
+      sharkYEnd = Number(enemyArray[i].getAttribute("y")) + 160;
       xCollision3 = false;
       yCollision3 = false;
       xCollision4 = false;
       yCollision4 = false;
-  }
-  window.requestAnimationFrame(checkForEnemyCollision);
+      if (Number(character.getAttribute("x")) > sharkX && Number(character.getAttribute("x")) < sharkXEnd) {
+        xCollision3 = true;
+      }
+      if (Number(character.getAttribute("y")) > sharkY && Number(character.getAttribute("y")) < sharkYEnd) {
+        yCollision3 = true;
+      }
+      if (Number(character.getAttribute("x")) + 80 < sharkXEnd && Number(character.getAttribute("x")) + 80 > sharkX) {
+        xCollision4 = true;
+      }
+      if (Number(character.getAttribute("y")) + 160 < sharkYEnd && Number(character.getAttribute("y")) + 160 > sharkY) {
+        yCollision4 = true;
+      }
+      if (xCollision3 == true && yCollision3 == true) {
+        lives = lives - .01; livesText.textContent = "Lives Left: " + lives.toFixed(0);
+      } else if (xCollision4 == true && yCollision4 == true) {
+        lives = lives - .01; livesText.textContent = "Lives Left: " + lives.toFixed(0);
+      } else if (xCollision3 == true && yCollision4 == true) {
+        lives = lives - .01; livesText.textContent = "Lives Left: " + lives.toFixed(0);
+      } else if (xCollision4 == true && yCollision3 == true) {
+        lives = lives - .01; livesText.textContent = "Lives Left: " + lives.toFixed(0);
+      } else {
+        xCollision3 = false;
+        yCollision3 = false;
+        xCollision4 = false;
+        yCollision4 = false;
+      }
+    }
+    window.requestAnimationFrame(checkForEnemyCollision);
 }
+
 function moveEnemy() {
-  var sharkX = Number(shark.getAttribute("x"));
-  var sharkY = Number(shark.getAttribute("y"));
-  if(Math.abs(sharkX - charXValue) > Math.abs(sharkY - charYValue)) {
-    if(sharkX > charXValue) {
-      shark.setAttribute("x", sharkX - 2);
-    }
-    else {
-      shark.setAttribute("x", sharkX + 2);
+  for(var i = 0; i < enemyArray.length; i++) {
+    var sharkX = Number(enemyArray[i].getAttribute("x"));
+    var sharkY = Number(enemyArray[i].getAttribute("y"));
+    if (Math.abs(sharkX - charXValue) > Math.abs(sharkY - charYValue)) {
+      if (sharkX > charXValue) {
+        enemyArray[i].setAttribute("x", sharkX - 2);
+      } else {
+        enemyArray[i].setAttribute("x", sharkX + 2);
+      }
+    } else if (sharkX == charXValue && sharkY == charYValue) {
+      lives = lives - .05;
+      livesText.textContent = "Lives Left: " + lives.toFixed(0);
+    } else {
+      if (sharkY > charYValue) {
+        enemyArray[i].setAttribute("y", sharkY - 2);
+      } else {
+        enemyArray[i].setAttribute("y", sharkY + 2);
+      }
     }
   }
-  else if(sharkX == charXValue && sharkY == charYValue)   {
-    console.log("The shark is perfectly aligned")
-  }
-  else {
-    if(sharkY > charYValue) {
-      shark.setAttribute("y", sharkY - 2);
-    }
-    else {
-      shark.setAttribute("y", sharkY + 2);
-    }
-  }
-  window.requestAnimationFrame(moveEnemy);
+    window.requestAnimationFrame(moveEnemy);
 }
+
 function endGame() {
+  if(won == false) {
     document.getElementById("main-canvas").pauseAnimations();
     gamePaused = true;
     document.getElementById("gameOver").setAttribute("opacity", 1);
@@ -166,11 +197,48 @@ function endGame() {
     gameOverText = document.getElementById("gameOverText");
     gameOverText.setAttribute("x", window.innerWidth / 2 - 300);
     gameOverText.setAttribute("y", window.innerHeight / 2);
+  }
+  else {
+    document.getElementById("main-canvas").pauseAnimations();
+    gamePaused = true;
+    document.getElementById("gameOver").setAttribute("opacity", 1);
+    timeStop = Date.now();
+    timeString = "  Time-taken: " + (timeStop - timeStart) / 1000 + " seconds";
+    winGameText = document.getElementById("winText");
+    winGameText.setAttribute("x", window.innerWidth / 2 - 300);
+    winGameText.setAttribute("y", window.innerHeight / 2);
+  }
+  for(var i = 0; i < enemyArray.length; i++) {
+    enemyArray[i].setAttribute("x", 1000000000000);
+  }
 }
+// function moveEnemy2() {
+//     var cloneX = Number(clone.getAttribute("x"));
+//     var cloneY = Number(clone.getAttribute("y"));
+//     if (Math.abs(cloneX - charXValue) > Math.abs(cloneY - charYValue)) {
+//         if (cloneX > charXValue) {
+//             clone.setAttribute("x", cloneX - 2);
+//         } else {
+//             clone.setAttribute("x", cloneX + 2);
+//         }
+//     } else if (cloneX == charXValue && cloneY == charYValue) {
+//         lives = lives - .05;
+//         livesText.textContent = "Lives Left: " + lives.toFixed(0);
+//     } else {
+//         if (cloneY > charYValue) {
+//             clone.setAttribute("y", cloneY - 2);
+//         } else {
+//             clone.setAttribute("y", cloneY + 2);
+//         }
+//     }
+//     window.requestAnimationFrame(moveEnemy2);
+// }
+
 
 checkForEnemyCollision();
 moveEnemy();
 checkForFoodCollision();
+//moveEnemy2();
 
 //You should uncomment this and run the code :) It makes a cool cascade
 // var array = [];
@@ -178,3 +246,4 @@ checkForFoodCollision();
 // array.push(i);
 // console.log(array);
 // }
+console.log(enemyArray);
--- replace.js
+++ replace.js
@@ -1,30 +1,31 @@
-    sharkX = Number(sharkEnemy.getAttribute("x"));
-    sharkXEnd = Number(sharkEnemy.getAttribute("x"))+ 80;
-    sharkY = Number(sharkEnemy.getAttribute("y"));
-    sharkYEnd = Number(sharkEnemy.getAttribute("y")) + 160;
-    xCollision3 = false;
-    yCollision3 = false;
-    xCollision4 = false;
-    yCollision4 = false;
-    if (Number(character.getAttribute("x")) > sharkX && Number(character.getAttribute("x")) < sharkXEnd) {
-        xCollision3 = true;
+function bobIsUpdating() {
+  console.log("MyName is BOB");
+  requestAnimationFrame(bobIsUpdating);
+}
+bobIsUpdating();
+//This is showing ANDORS how to Updateroniz
+
+
+function moveEnemy() {
+  for(var i = 0; i < enemyArray.length; i++) {
+    var sharkX = Number(enemyArray[i].getAttribute("x"));
+    var sharkY = Number(enemyArray[i].getAttribute("y"));
+    if (Math.abs(sharkX - charXValue) > Math.abs(sharkY - charYValue)) {
+      if (sharkX > charXValue) {
+        enemyArray[i].setAttribute("x", sharkX - 2);
+      } else {
+        enemyArray[i].setAttribute("x", sharkX + 2);
+      }
+    } else if (sharkX == charXValue && sharkY == charYValue) {
+      lives = lives - .05;
+      livesText.textContent = "Lives Left: " + lives.toFixed(0);
+    } else {
+      if (sharkY > charYValue) {
+        enemyArray[i].setAttribute("y", sharkY - 2);
+      } else {
+        enemyArray[i].setAttribute("y", sharkY + 2);
+      }
     }
-    if (Number(character.getAttribute("y")) > sharkY && Number(character.getAttribute("y")) < sharkYEnd) {
-        yCollision3 = true;
-    }
-    if (Number(character.getAttribute("x")) + 80 < sharkXEnd && Number(character.getAttribute("x")) + 80 > sharkX) {
-        xCollision4 = true;
-    }
-    if (Number(character.getAttribute("y")) + 160 < sharkYEnd && Number(character.getAttribute("y")) + 160 > sharkY) {
-        yCollision4 = true;
-    }
-    if (xCollision3 == true && yCollision3 == true) {   }
-    else if (xCollision4 == true && yCollision4 == true) {    }
-    else if(xCollision3 == true && yCollision4 == true) {    }
-    else if(xCollision4 == true && yCollision3 == true) {    }
-    else {
-        xCollision3 = false;
-        yCollision3 = false;
-        xCollision4 = false;
-        yCollision4 = false;
-    }
+  }
+    window.requestAnimationFrame(moveEnemy);
+}
